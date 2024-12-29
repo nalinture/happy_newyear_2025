40d5ad22a7c740cb95ad8c9e476c8de88e2c48de
nalinture <68896641+nalinture@users.noreply.github.com>
2024-12-29 21:18:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+      backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "aws_lambda"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "terraform_tutorial"
+         }
+     }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_s3_bucket" "bucket" {
+  bucket = "birthdaybucket05"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  for_each = fileset("./", "*")
+  bucket = data.aws_s3_bucket.bucket.id
+  key    = each.value
+  source = "./${each.value}"
+  acl = "public-read"
+  etag   = filemd5("./${each.value}")
+}
+
+resource "aws_s3_bucket_website_configuration" "webconfig" {
+  bucket = data.aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "smile.html"
+  }
+
+}
+
+resource "aws_s3_bucket_policy" "bucketpolicy" {
+  bucket = data.aws_s3_bucket.bucket.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "${data.aws_s3_bucket.bucket.arn}/*"
+        }
+    ]
+
+  })
+}
